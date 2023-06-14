31d0d974b2f5118314c1dbd573c56a78bf81c1c8
gmiserani <gabrielamiserani@uol.com.br>
2023-06-13 21:33:27 -0300

    adicionando pesquisas por paginas

--- tp2virtual.c
+++ tp2virtual.c
@@ -9,49 +9,55 @@ pedidas no enunciado do trabalho*/
 int lidas = 0;
 int escritas = 0;
 int totalUsed = 0;
+int faults = 0;
 
 typedef struct Page {
 	unsigned addr;
-	Page *next;	
-}Page;
+	struct Page_t *next;	
+}Page_t;
 
-Page *first, *last;
-Page array[] =
+Page_t *first, *last;
 
 void AddNew(unsigned addr){
-    Page *new = (Page*)malloc(sizeof(Page));
-    new->addr = addr;
-    new->next = NULL;
-
+    Page_t *current = (Page_t*)malloc(sizeof(Page_t));
+    current->addr = addr;
+    current->next = NULL;
     if(totalUsed == 0){
-        first = new;
-        last = new;
+        first = current;
+        last = current->next;
     }
     else{
-        last->next = new;
-        last = new;
+        last->next = current;
+        last = current;
     }
     totalUsed++;
     escritas++;
 }
 
 void writeAddress(addr, sub, totalPages){
-    if(totalUsed < totalPages){
-        AddNew(addr);
-    }else{
-        if(strcmp(sub, "lru") == 0){
+    if(strcmp(sub, "lru") == 0){
+        LRU(addr);
+    }
+    else if(strcmp(sub, "2a") == 0){
+        FIFO(addr);
+    }
+    else if(strcmp(sub, "random") == 0){
+        secondChance(addr);
+    }
+    else if(strcmp(sub, "fifo") == 0){
+        randomAlg(addr);
+    }
+}
 
-        }
-        else if(strcmp(sub, "2a") == 0){
-            
-        }
-        else if(strcmp(sub, "random") == 0){
-
-        }
-        else if(strcmp(sub, "fifo") == 0){
-            
+Page_t* AlreadyWritten(address){
+    Page_t *aux = first;
+    while (aux->next != NULL || aux->next != first)
+    {
+        if(aux->addr == address){
+            return aux;
         }
     }
+    return NULL;
 }
 
 int main(int argc, char* argv[]){
@@ -60,17 +66,28 @@ int main(int argc, char* argv[]){
     char rw;
     char *sub = argv[1];
     file = fopen(argv[2], "r");
-    int pag = argv[3];
-    int mem = argv[4]; 
+    int pag = atoi(argv[3]);
+    int mem = atoi(argv[4]); 
     int totalPages = mem/pag;
     while(fscanf(file,"%x %c",&addr,&rw) != -1){
         //printf("%x",addr);
         if(rw == 'W' || rw == 'w'){
-			writeAddress(addr, sub, totalPages);
+            if(AlreadyWritten(addr) == NULL){
+                if(totalUsed < totalPages){
+                    AddNew(addr);
+                }
+                else{
+			        writeAddress(addr, sub, totalPages);
+                }faults++;
+            }
 		}
         else if(rw == 'R' || rw == 'r'){
-            if(!AddresExists(addr)){
-                writeAddress(addr, sub, totalPages);
+            if(AlreadyWritten(addr) == NULL){
+                if(totalUsed < totalPages){
+                    AddNew(addr);
+                }else{
+                    writeAddress(addr, sub, totalPages);
+                }faults++;
             }
             lidas++;
         }
